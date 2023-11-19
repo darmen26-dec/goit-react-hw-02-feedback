e4b82561b5e54de27657c3b764a57a1e2088d295
darmen26-dec <dariamendelewska@gmail.com>
2023-11-19 20:05:45 +0100

    hw-02-feedback

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -20,16 +20,3 @@ Statistics.propTypes = {
 };
 
 export default Statistics;
-
-
-
-//    <div className={css.statisticsContainer}>
-//      <h3 className={css.statisticsTitle}>Statistics</h3>
-//      <ul className={css.statisticsList}>
-//        <li>Good: {good}</li>
-//        <li>Neutral: {neutral} </li>
-//        <li>Bad: {bad}</li>
-//        <li>Total: {total}</li>
-//        <li>Positive feedback: {positivePercentage}%</li>
-//      </ul>
-//    </div>;
